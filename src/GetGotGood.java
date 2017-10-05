172f632fc2bca47162f3b8c39253e19b5505fca0
alazarg8 <hi@alazarg8.me>
2017-10-05 16:59:05 -0400

    Initial commit

---
+++ src/GetGotGood.java
@@ -0,0 +1,29 @@
+public class GetGotGood {
+
+    public static void main(String[] args) {
+
+       for (int i = 1; i <=100;i++)
+
+
+            if ((i % 3 == 0))
+
+            {
+                System.out.println("Get it");
+
+                if ((i % 5 == 0))
+
+                {
+                    System.out.println("Got it");
+                }
+
+                if ((i % 10 == 0))
+
+                {
+                    System.out.println("Good");
+                }
+
+                    else if (i%3!=0 && i%5!=0 && i%10!=0 );
+        System.out.println("Get it, Got out, Good");      }
+}
+
+            }
